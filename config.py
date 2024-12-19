000823a435d30beaca9b8bfb1f518b18b93eba4b
Usynin Sergei <usynin.s00@mail.ru>
2024-12-19 16:18:56 +0300

    Just in case.

--- config.py
+++ config.py
@@ -3,5 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# got from MetaMask
 PRIVATE_KEY = os.getenv('PRIVATE_KEY')
+
+# got as API_KEY in https://developer.metamask.io/
 INFURA_API_KEY = os.getenv('INFURA_API_KEY')
